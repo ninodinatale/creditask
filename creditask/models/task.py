eeda46b5aefc38f4d13bca00b9a41afe67ef0f9e
Nino Di Natale <nino.dinatale@talent-factory.ch>
2020-05-23 15:48:15 +0200

    fixed type hint

--- creditask/models/task.py
+++ creditask/models/task.py
@@ -22,8 +22,8 @@ class Task(BaseModel):
                              max_length=64, default=State.TO_DO)
     factor: int = models.IntegerField(default=0)
     user: User = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
-    period_start: str = models.DateField(default=datetime.datetime.now)
-    period_end: str = models.DateField(default=datetime.datetime.now)
+    period_start: datetime.date = models.DateField(default=datetime.datetime.now)
+    period_end: datetime.date = models.DateField(default=datetime.datetime.now)
     done: bool = models.BooleanField(default=False)
     approvals = models.ManyToManyField(Approval, related_name='tasks')
 
