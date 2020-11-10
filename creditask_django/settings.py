5e31f3d9ee87cb060046433d8cdbc695c7e710b3
Nino Di Natale <nino.dinatale@talent-factory.ch>
2020-11-10 13:49:04 +0100

    reverted unwanted changes

--- creditask_django/settings.py
+++ creditask_django/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-SECRET_KEY = 'abc' #os.environ['SECRET_KEY']
+SECRET_KEY = os.environ['SECRET_KEY']
 
-DEBUG = False # os.environ['DEBUG']
+DEBUG = os.environ['DEBUG']
 
 ALLOWED_HOSTS = ['*']
 
