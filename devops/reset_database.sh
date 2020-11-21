efa43dd6c8a87e635980912f92ecd5180bfcf44a
Nino Di Natale <nino.dinatale@talent-factory.ch>
2020-11-21 18:06:36 +0100

    fixed stuff

--- devops/reset_database.sh
+++ devops/reset_database.sh
@@ -1,12 +1,13 @@
 set -e
+source env/bin/activate
 dropdb creditask
 createdb creditask
 printf "\033[1;34mdatabase creditask dropped and recreated\e[39m\n"
 rm -f ./creditask/migrations/*.py
 touch ./creditask/migrations/__init__.py
-./manage.py makemigrations
-./manage.py migrate
+python3 ./manage.py makemigrations
+python3 ./manage.py migrate
 printf "\033[1;34mmigrations ran\e[39m\n"
-./manage.py populate_dev_data
+python3 ./manage.py populate_dev_data
 printf "\033[1;34mpopulated dev data\e[39m\n"
 printf "\033[1;32mdatabase creditask has been reset, migrations have been run and dev data has been created\e[39m\n"
