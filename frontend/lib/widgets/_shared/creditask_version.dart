a6c6883f42543ef901d859be005075c260f0c0ec
Nino Di Natale <nino.dinatale@talent-factory.ch>
2020-12-16 15:13:29 +0100

    added display of version+build number

---
+++ frontend/lib/widgets/_shared/creditask_version.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:package_info/package_info.dart';
+
+class CreditaskVersion extends StatelessWidget {
+  final TextStyle style;
+
+  CreditaskVersion({this.style});
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<PackageInfo>(
+        future: PackageInfo.fromPlatform(),
+        builder: (BuildContext context, AsyncSnapshot<PackageInfo> snapshot) {
+          if (snapshot.connectionState == ConnectionState.done &&
+              snapshot.hasData) {
+            String version = snapshot.data.version;
+            String buildNumber = snapshot.data.buildNumber;
+            return Text('v$version+$buildNumber', style: this.style);
+          } else {
+            return Text('', style: this.style);
+          }
+        });
+  }
+}
